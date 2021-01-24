f4f979f4b8337805ba74f34f613ba81536f23c6a
Andrews_code <kov.andreas.30@gmail.com>
2021-01-24 11:53:18 +0200

    change in secound_file

--- dir_2/secound_file.py
+++ dir_2/secound_file.py
@@ -1,4 +1,9 @@
-for i in range(34, 0, -2):
+for i in range(34, 0, -5):
         print(i ** 2)
 print("Bye.")
 
+#creating iterator
+
+iterator1 = (x ** 2 for x in [4, 2, 5, 3])
+
+print(next(iterator1))
